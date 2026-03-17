4cc3f62d260d5ef095427a1550155ac07b87a074
Aidar <ab362009psa1@gmail.com>
2026-03-17 16:31:57 +0600

    Update seed test data

--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,20 @@
 INSERT INTO barbers (name)
 VALUES
-  ('Azamat'),
-  ('Beksultan'),
-  ('Daniyar')
+  ('AzamatTEST'),
+  ('BeksultanTEST'),
+  ('DastanTEST'),
+  ('ErlanTEST'),
+  ('KairatTEST'),
+  ('MaksatTEST'),
+  ('NurlanTEST'),
+  ('RuslanTEST'),
+  ('SamatTEST'),
+  ('DaniyarTEST')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO services (name, duration_minutes, price)
 VALUES
-  ('Classic haircut', 30, 500),
-  ('Fade haircut', 45, 700),
-  ('Beard trim', 20, 400)
+  ('Classic haircutTEST', 30, 500),
+  ('Fade haircutTEST', 45, 700),
+  ('Beard trimTEST', 20, 400)
 ON CONFLICT DO NOTHING;
