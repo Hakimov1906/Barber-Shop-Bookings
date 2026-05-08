5ef6ed60e58418b301110747862b9e50a8c75106
Timur Djyldyzbekov <loltimurpro@gmail.com>
2026-05-08 19:15:30 +0600

    Add migration to allow Russian phone format (+7) for users and admins

--- barbershop-booking-api/db/migrations/1775689200000_allow_ru_phone_format.js
+++ barbershop-booking-api/db/migrations/1775689200000_allow_ru_phone_format.js
@@ -36,20 +36,3 @@ exports.down = async (pgm) => {
       CHECK (phone ~ '^\\+996\\d{9}$');
   `);
 };
-
-exports.down = async (pgm) => {
-  // Revert to Kyrgyzstan-only format
-  await pgm.sql(`
-    ALTER TABLE users DROP CONSTRAINT IF EXISTS users_phone_format_chk;
-    ALTER TABLE users
-      ADD CONSTRAINT users_phone_kg_format_chk
-      CHECK (phone ~ '^\\+996\\d{9}$');
-  `);
-
-  await pgm.sql(`
-    ALTER TABLE admins DROP CONSTRAINT IF EXISTS admins_phone_format_chk;
-    ALTER TABLE admins
-      ADD CONSTRAINT admins_phone_kg_format_chk
-      CHECK (phone ~ '^\\+996\\d{9}$');
-  `);
-};
